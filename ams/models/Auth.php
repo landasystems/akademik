dab96ca4bd88ef71ecf019e7aefa521fb418aa1b
x1n73ct <x1n73ct@x1n73ct>
2015-10-03 08:46:10 +0700

    Merge origin/master
    Conflicts:
    	tpl/r_wo-masuk/index.html

--- ams/models/Auth.php
+++ ams/models/Auth.php
@@ -108,49 +108,49 @@ class Auth extends CActiveRecord {
 
         return array(
             array('label' => '<span class="icon16 icomoon-icon-screen"></span>Halaman Depan', 'url' => array('/dashboard')),
-            array('visible' => landa()->checkAccess('User', 'r'), 'label' => '<span class="icon16 icomoon-icon-user-3"></span>User', 'url' => array('/user'), 'auth_id' => 'User'),
+            array('visible' => landa()->checkAccess('User', 'r'), 'auth_id' => 'User', 'label' => '<span class="icon16 icomoon-icon-user-3"></span>User', 'url' => array('/user'), 'crud' => array("r" => 1)),
             array('visible' => landa()->checkAccess('GroupTeacher', 'r') || landa()->checkAccess('Teacher', 'r'), 'label' => '<span class="icon16  entypo-icon-contact"></span>Guru', 'url' => array('#'), 'submenuOptions' => array('class' => 'sub'), 'items' => array(
-                    array('visible' => landa()->checkAccess('GroupTeacher', 'r'), 'label' => '<span class="icon16 entypo-icon-users"></span>Group Guru', 'url' => url('landa/roles/teacher'), 'auth_id' => 'GroupTeacher'),
-                    array('visible' => landa()->checkAccess('Teacher', 'r'), 'label' => '<span class="icon16  entypo-icon-user"></span>Guru', 'url' => url('user/teacher'), 'auth_id' => 'Teacher'),
+                    array('visible' => landa()->checkAccess('GroupTeacher', 'r'), 'auth_id' => 'GroupTeacher', 'label' => '<span class="icon16 entypo-icon-users"></span>Group Guru', 'url' => url('landa/roles/teacher'), 'crud' => array("r" => 1)),
+                    array('visible' => landa()->checkAccess('Teacher', 'r'), 'auth_id' => 'Teacher', 'label' => '<span class="icon16  entypo-icon-user"></span>Guru', 'url' => url('user/teacher'), 'crud' => array("r" => 1)),
                 )),
             array('visible' => landa()->checkAccess('GroupStudent', 'r') || landa()->checkAccess('Student', 'r'), 'label' => '<span class="icon16  entypo-icon-contact"></span>Murid', 'url' => array('#'), 'submenuOptions' => array('class' => 'sub'), 'items' => array(
-                    array('visible' => landa()->checkAccess('GroupStudent', 'r'), 'label' => '<span class="icon16 entypo-icon-users"></span>Group Murid', 'url' => url('landa/roles/student'), 'auth_id' => 'GroupStudent'),
-                    array('visible' => landa()->checkAccess('Student', 'r'), 'label' => '<span class="icon16  entypo-icon-user"></span>Murid', 'url' => url('user/student'), 'auth_id' => 'Student'),
+                    array('visible' => landa()->checkAccess('GroupStudent', 'r'), 'auth_id' => 'GroupStudent', 'label' => '<span class="icon16 entypo-icon-users"></span>Group Murid', 'url' => url('landa/roles/student'), 'crud' => array("r" => 1)),
+                    array('visible' => landa()->checkAccess('Student', 'r'), 'auth_id' => 'Student', 'label' => '<span class="icon16  entypo-icon-user"></span>Murid', 'url' => url('user/student'), 'crud' => array("r" => 1)),
                 )),
-            array('visible' => landa()->checkAccess('Download', 'r'), 'label' => '<span class="icon16 minia-icon-download"></span>Dokumen', 'url' => array('/downloadCategory'), 'auth_id' => 'Download'),
+            array('visible' => landa()->checkAccess('Download', 'r'), 'auth_id' => 'Download', 'label' => '<span class="icon16 minia-icon-download"></span>Dokumen', 'url' => array('/downloadCategory')),
             array('visible' => landa()->checkAccess('SchoolYear', 'r') || landa()->checkAccess('Classroom', 'r') || landa()->checkAccess('UserClassroom', 'r'), 'label' => '<span class="icon16 typ-icon-cog"></span>Akademik', 'url' => array('/User'), 'submenuOptions' => array('class' => 'sub'), 'items' => array(
-                    array('visible' => landa()->checkAccess('SchoolYear', 'r'), 'label' => '<span class="icon16 minia-icon-calendar"></span>Tahun Ajaran', 'url' => array('/schoolYear'), 'auth_id' => 'SchoolYear'),
-                    array('visible' => landa()->checkAccess('Classroom', 'r'), 'label' => '<span class="icon16 minia-icon-office"></span>Kelas', 'url' => array('/classroom'), 'auth_id' => 'Classroom'),
-                    array('visible' => landa()->checkAccess('UserClassroom', 'r'), 'label' => '<span class="icon16 typ-icon-users"></span>Penempatan Siswa', 'url' => array('/userClassroom'), 'auth_id' => 'UserClassroom'),
+                    array('visible' => landa()->checkAccess('SchoolYear', 'r'), 'auth_id' => 'SchoolYear', 'label' => '<span class="icon16 minia-icon-calendar"></span>Tahun Ajaran', 'url' => array('/schoolYear'), 'crud' => array("r" => 1)),
+                    array('visible' => landa()->checkAccess('Classroom', 'r'), 'auth_id' => 'Classroom', 'label' => '<span class="icon16 minia-icon-office"></span>Kelas', 'url' => array('/classroom'), 'crud' => array("r" => 1)),
+                    array('visible' => landa()->checkAccess('UserClassroom', 'r'), 'auth_id' => 'UserClassroom', 'label' => '<span class="icon16 typ-icon-users"></span>Penempatan Siswa', 'url' => array('/userClassroom'), 'crud' => array("r" => 1)),
                 )),
             array('visible' => landa()->checkAccess('ExamCategory', 'r') || landa()->checkAccess('Exam', 'r') || landa()->checkAccess('Test', 'r') || landa()->checkAccess('TestResult', 'r'), 'label' => '<span class="icon16 minia-icon-book"></span>Ujian', 'url' => array('/User'), 'submenuOptions' => array('class' => 'sub'), 'items' => array(
-                    array('visible' => landa()->checkAccess('ExamCategory', 'r'), 'label' => '<span class="icon16 iconic-icon-list-nested"></span>Kategori', 'url' => array('/examCategory'), 'auth_id' => 'ExamCategory'),
-                    array('visible' => landa()->checkAccess('Exam', 'r'), 'label' => '<span class="icon16 typ-icon-views"></span>Soal', 'url' => array('/exam'), 'auth_id' => 'Exam'),
-                    array('visible' => landa()->checkAccess('Test', 'r'), 'label' => '<span class="icon16 wpzoom-timer"></span>Jadwal', 'url' => array('/test'), 'auth_id' => 'Test'),
-                    array('visible' => landa()->checkAccess('TestResult', 'r'), 'label' => '<span class="icon16 minia-icon-checked"></span>Hasil Ujian', 'url' => array('/testResult'), 'auth_id' => 'TestResult'),
+                    array('visible' => landa()->checkAccess('ExamCategory', 'r'), 'auth_id' => 'ExamCategory', 'label' => '<span class="icon16 iconic-icon-list-nested"></span>Kategori', 'url' => array('/examCategory'), 'crud' => array("r" => 1)),
+                    array('visible' => landa()->checkAccess('Exam', 'r'), 'auth_id' => 'Exam', 'label' => '<span class="icon16 typ-icon-views"></span>Soal', 'url' => array('/exam'), 'crud' => array("r" => 1)),
+                    array('visible' => landa()->checkAccess('Test', 'r'), 'auth_id' => 'Test', 'label' => '<span class="icon16 wpzoom-timer"></span>Jadwal', 'url' => array('/test'), 'crud' => array("r" => 1)),
+                    array('visible' => landa()->checkAccess('TestResult', 'r'), 'auth_id' => 'TestResult', 'label' => '<span class="icon16 minia-icon-checked"></span>Hasil Ujian', 'url' => array('/testResult'), 'crud' => array("r" => 1)),
                 )),
 //                array('visible' => landa()->checkAccess('Ongoing', 'r'), 'label' => '<span class="icon16 cut-icon-arrow-right"></span>Mulai Ujian', 'url' => array('/test/ongoing'), 'auth_id' => 'Ongoing', 'submenuOptions' => array('class' => 'sub'), 'items' => array(
 //                    )),
             array('visible' => landa()->checkAccess('Sms', 'r'), 'label' => '<span class="icon16 wpzoom-phone-3"></span>SMS', 'url' => array('#'), 'submenuOptions' => array('class' => 'sub'), 'items' => array(
-                    array('visible' => landa()->checkAccess('Sms', 'r'), 'label' => '<span class="icon16 icomoon-icon-key-2"></span>Keyword', 'url' => url('/smsKeyword'), 'auth_id' => 'Customer'),
-                    array('visible' => landa()->checkAccess('Sms', 'r'), 'label' => '<span class="icon16 entypo-icon-comment"></span>Outbox', 'url' => url('/sms/outbox'), 'auth_id' => 'Sms'),
-                    array('visible' => landa()->checkAccess('Sms', 'r'), 'label' => '<span class="icon16 entypo-icon-inbox"></span>Inbox & Sentitems', 'url' => url('/sms'), 'auth_id' => 'Sms'),
-                    array('visible' => landa()->checkAccess('Sms', 'r'), 'label' => '<span class="icon16 icomoon-icon-mobile-2"></span>Log Status Modem', 'url' => url('/sms/logModem'), 'auth_id' => 'Sms'),
+                    array('visible' => landa()->checkAccess('smsKeyword', 'r'), 'auth_id' => 'smsKeyword', 'label' => '<span class="icon16 icomoon-icon-key-2"></span>Keyword', 'url' => url('/smsKeyword'), 'crud' => array("r" => 1)),
+                    array('visible' => landa()->checkAccess('outbox', 'r'), 'auth_id' => 'outbox', 'label' => '<span class="icon16 entypo-icon-comment"></span>Outbox', 'url' => url('/sms/outbox'), 'crud' => array("r" => 1)),
+                    array('visible' => landa()->checkAccess('Sms', 'r'), 'auth_id' => 'Sms', 'label' => '<span class="icon16 entypo-icon-inbox"></span>Inbox & Sentitems', 'url' => url('/sms'), 'crud' => array("r" => 1)),
+                    array('visible' => landa()->checkAccess('logModem', 'r'), 'auth_id' => 'logModem', 'label' => '<span class="icon16 icomoon-icon-mobile-2"></span>Log Status Modem', 'url' => url('/sms/logModem'), 'crud' => array("r" => 1)),
                 )),
             array('visible' => landa()->checkAccess('ReportMonth', 'r') || landa()->checkAccess('ReportDay', 'r'), 'label' => '<span class="icon16 cut-icon-list"></span>Absensi', 'url' => array('#'), 'submenuOptions' => array('class' => 'sub'), 'items' => array(
-                    array('visible' => landa()->checkAccess('Holiday', 'r'), 'label' => '<span class="icon16 brocco-icon-calendar"></span>Hari Libur', 'url' => url('/siteConfigHoliday'), 'auth_id' => 'ReportMonth'),
-                    array('visible' => landa()->checkAccess('ReportMonth', 'r'), 'label' => '<span class="icon16 brocco-icon-calendar"></span>Rekap', 'url' => url('/ReportAbsent'), 'auth_id' => 'ReportMonth'),
+                    array('visible' => landa()->checkAccess('Holiday', 'r'), 'auth_id' => 'Holiday', 'label' => '<span class="icon16 brocco-icon-calendar"></span>Hari Libur', 'url' => url('/siteConfigHoliday'), 'crud' => array("r" => 1)),
+                    array('visible' => landa()->checkAccess('ReportMonth', 'r'), 'auth_id' => 'ReportMonth', 'label' => '<span class="icon16 brocco-icon-calendar"></span>Rekap', 'url' => url('/ReportAbsent'), 'crud' => array("r" => 1)),
                 )),
             array('visible' => landa()->checkAccess('Report_Exam', 'r') || landa()->checkAccess('Report_Userlist', 'r'), 'label' => '<span class="icon16 cut-icon-printer-2"></span>Laporan', 'url' => array('#'), 'submenuOptions' => array('class' => 'sub'), 'items' => array(
-                    array('visible' => landa()->checkAccess('Sms', 'r'), 'label' => '<span class="icon16 icomoon-icon-mail"></span>Pesan Terkirim', 'url' => url('/report/sentItem'), 'auth_id' => 'Customer'),
-                    array('visible' => landa()->checkAccess('Report_Exam', 'r'), 'label' => '<span class="icon16 entypo-icon-list"></span>Nilai Ujian', 'url' => array('/report/examReport'), 'auth_id' => 'Report_Exam'),
-                    array('visible' => landa()->checkAccess('Report_Day', 'r'), 'label' => '<span class="icon16  brocco-icon-window"></span>Absensi Harian', 'url' => url('/ReportAbsent/ReportHarian'), 'auth_id' => 'Report_Day'),
+                    array('visible' => landa()->checkAccess('Sms', 'r'), 'auth_id' => 'Customer', 'label' => '<span class="icon16 icomoon-icon-mail"></span>Pesan Terkirim', 'url' => url('/report/sentItem'), 'crud' => array("r" => 1)),
+                    array('visible' => landa()->checkAccess('Report_Exam', 'r'), 'auth_id' => 'Report_Exam', 'label' => '<span class="icon16 entypo-icon-list"></span>Nilai Ujian', 'url' => array('/report/examReport'), 'crud' => array("r" => 1)),
+                    array('visible' => landa()->checkAccess('Report_Day', 'r'), 'auth_id' => 'Report_Day', 'label' => '<span class="icon16  brocco-icon-window"></span>Absensi Harian', 'url' => url('/ReportAbsent/ReportHarian'), 'crud' => array("r" => 1)),
                 //array('label' => '<span class="icon16 entypo-icon-list"></span>Classroom', 'url' => array('#')),
 //                        array('visible'=>landa()->checkAccess('Report_Userlist','r'),'label' => '<span class="icon16 entypo-icon-list"></span>Daftar User', 'url' => array('/user'),'auth_id'=>'Report_Userlist'),
 //                        array('visible'=>landa()->checkAccess('Report_Classroomlist','r'),'label' => '<span class="icon16 entypo-icon-list"></span>Daftar Kelas', 'url' => array('/classroom'),'auth_id'=>'Report_ClassroomList'),
                 )),
             array('visible' => landa()->checkAccess('Chart_ExamComparison', 'r'), 'label' => '<span class="icon16 icomoon-icon-bars"></span>Chart', 'url' => array('/User'), 'submenuOptions' => array('class' => 'sub'), 'items' => array(
-                    array('visible' => landa()->checkAccess('Chart_ExamComparison', 'r'), 'label' => '<span class="icon16 entypo-icon-list"></span>Ujian Pembanding', 'url' => array('/chart/examComparison'), 'auth_id' => 'Chart_ExamComparison'),
+                    array('visible' => landa()->checkAccess('Chart_ExamComparison', 'r'), 'auth_id' => 'Chart_ExamComparison', 'label' => '<span class="icon16 entypo-icon-list"></span>Ujian Pembanding', 'url' => array('/chart/examComparison'), 'crud' => array("r" => 1)),
                 )),
         );
     }
